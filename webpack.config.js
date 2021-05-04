7457d004bcb8a54f4929b38839103d6dcd2899d7
tamakiii <tamakiii@users.noreply.github.com>
2021-05-05 01:49:33 +0900

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        // exclude: /node_modules/
+      },
     ],
   },
   resolve: {
