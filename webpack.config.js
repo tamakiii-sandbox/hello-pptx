9bf77b86b1b5b3171e3d4e5fda682fdc688d7f52
tamakiii <tamakiii@users.noreply.github.com>
2021-05-05 01:49:51 +0900

    Revert "add babel-loader"
    This reverts commit 7457d004bcb8a54f4929b38839103d6dcd2899d7.

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,6 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-        // exclude: /node_modules/
-      },
     ],
   },
   resolve: {
